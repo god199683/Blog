72fb94a22e0ba519491ad6aa7dbca6de59e4bce8
ciel <god199683@gmail.com>
2026-05-06 00:35:56 +0900

    Remove fixed blog owner name

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 window.BLOG_CONFIG = {
   blogTitle: "Sky Blog",
-  ownerName: "god199683",
   supabaseUrl: "https://ipylqxcmajrwtvvmrvfy.supabase.co",
   supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlweWxxeGNtYWpyd3R2dm1ydmZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5OTM2ODMsImV4cCI6MjA5MzU2OTY4M30.v0s8RWMeMwqHGdL_1qey--PQGq67x0ltTojSxfV7T3M",
   tableName: "posts"
