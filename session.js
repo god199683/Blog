789324ef81e491c5912bd3b26958fb46913498d6
ciel <god199683@gmail.com>
2026-05-21 22:05:44 +0900

    Bust APK download cache

--- session.js
+++ session.js
@@ -5,7 +5,7 @@
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlweWxxeGNtYWpyd3R2dm1ydmZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5OTM2ODMsImV4cCI6MjA5MzU2OTY4M30.v0s8RWMeMwqHGdL_1qey--PQGq67x0ltTojSxfV7T3M";
   const REFRESH_WINDOW_MS = 60 * 1000;
   const AWAY_LOCK_KEY = "blog.away.locked";
-  const APK_DOWNLOAD_PATH = "./Blog.apk";
+  const APK_DOWNLOAD_PATH = "./Blog.apk?v=1.0.3";
 
   function readSession() {
     try {
